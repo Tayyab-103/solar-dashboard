827eefe9162bf740de80ceb0cf2227b925a116fb
Tayyab-103 <tayyabhameed103@gmail.com>
2025-01-06 16:37:14 +0500

    integrate sidebar and also integrate theme provider

--- radiabase_frontened/src/app/layout.tsx
+++ radiabase_frontened/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
+
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +31,23 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* {children} */}
+          <SidebarProvider>
+            <AppSidebar />
+            <main>
+              <SidebarTrigger />
+              {children}
+            </main>
+          </SidebarProvider>
+        </ThemeProvider>
+
+        {/* {children} */}
       </body>
     </html>
   );
--- radiabase_frontened/src/app/page.tsx
+++ radiabase_frontened/src/app/page.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import Dashboard from "./dashboard/page";
+
 
 
 export default function Home() {
-  return (
-  <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
 
-    <h1 className="text-3xl font-bold underline text-white">Hello World</h1>
-     <Button variant="outline">Button</Button>
-  </div>
+  return (
+    <div>
+      <Dashboard />
+    </div>
   );
 }
